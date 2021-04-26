df2e2caa774ffdc7c82d0fe60dea7d577012a3bd
Jeffer Cardenas <jecardenas611@misena.edu.co>
2021-04-26 13:29:43 -0500

    se arregla el buscar cliente en citas no programadas

--- controlador/citaControl.php
+++ controlador/citaControl.php
@@ -41,7 +41,8 @@
             break;
 
         case 'buscarMascotas':
-            $resultado = $dCita->buscarMascota($_SESSION['identificacion']);
+
+            $resultado = $dCita->buscarMascota($cedula);
             echo json_encode($resultado);
             break;
         
