6588aa8f85f0400c8539c47f11ab78af7bb6a5e7
OscarAstudillo <osastudillor.iss@gmail.com>
2021-05-19 18:51:21 -0500

    ajsute en precio del servicio

--- js/agendarCita/agendaCita.js
+++ js/agendarCita/agendaCita.js
@@ -284,7 +284,7 @@ function listarServicios() {
                         servicio.idServicio +
                         ">" +
                         "<span id='tipoServicio'>" +
-                        servicio.serTipo + "<span id='precio'>"+"$"+servicio.serPrecio.toFixed(1)+ "</span>"+
+                        servicio.serTipo + "<span id='precio'>"+"$"+number_format(servicio.serPrecio,2)+ "</span>"+
                         "</span>" +
                         "<p class='form-check-label'>" +
                         "<small class='serDescripcion'><p>" +
@@ -301,6 +301,28 @@ function listarServicios() {
             console.log(ex.responseText);
         },
     });
+}
+function number_format(amount, decimals) {
+
+    amount += ''; // por si pasan un numero en vez de un string
+    amount = parseFloat(amount.replace(/[^0-9\.]/g, '')); // elimino cualquier cosa que no sea numero o punto
+
+    decimals = decimals || 0; // por si la variable no fue fue pasada
+
+    // si no es un numero o es igual a cero retorno el mismo cero
+    if (isNaN(amount) || amount === 0) 
+        return parseFloat(0).toFixed(decimals);
+
+    // si es mayor o menor que cero retorno el valor formateado como numero
+    amount = '' + amount.toFixed(decimals);
+
+    var amount_parts = amount.split('.'),
+        regexp = /(\d+)(\d{3})/;
+
+    while (regexp.test(amount_parts[0]))
+        amount_parts[0] = amount_parts[0].replace(regexp, '$1' + ',' + '$2');
+
+    return amount_parts.join('.');
 }
 
 function listarHorasDisponibles(fechaEscogida) {
